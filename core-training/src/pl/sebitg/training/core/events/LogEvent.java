7c941bc7508020d942bf2e2c0a454809ff9d1bd9
Sebastian Mekal <sebitg@gmail.com>
2014-12-09 00:18:22 +0100

    Simple event

---
+++ core-training/src/pl/sebitg/training/core/events/LogEvent.java
@@ -0,0 +1,12 @@
+package pl.sebitg.training.core.events;
+
+/**
+ * This is just a POJO. Make it springable! :D
+ * @author s.mekal
+ *
+ */
+public class LogEvent {
+
+	private String message;
+	
+}
