ca5e348371a76d39d6da39cb2c5a8c66f9d7c00f
Sebastian Mekal <sebitg@gmail.com>
2014-12-08 23:43:55 +0100

    Resolve of task1

--- core-training/src/pl/sebitg/training/core/Main.java
+++ core-training/src/pl/sebitg/training/core/Main.java
@@ -1,15 +1,24 @@
 package pl.sebitg.training.core;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import pl.sebitg.training.core.sampleclasses.SomeInterface;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		ApplicationContext ctx;		//Initialize application context
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/resources/application-context.xml");		//Initialize application context
 		
 		//Get your bean here, and make some action...
+		System.out.println(ctx.containsBean("someBeanDoesNotExist"));
 		
+		SomeInterface bean = ctx.getBean(SomeInterface.class);
+		bean.foo();
+		
+		((AbstractApplicationContext)ctx).registerShutdownHook();	
 	}
 
 }
