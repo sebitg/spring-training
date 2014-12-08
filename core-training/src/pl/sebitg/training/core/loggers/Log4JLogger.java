e19c9d56718dbe7b918e533faf5d14742cad192f
Sebastian Mekal <sebitg@gmail.com>
2014-12-09 00:11:15 +0100

    Resolve of task2

--- core-training/src/pl/sebitg/training/core/loggers/Log4JLogger.java
+++ core-training/src/pl/sebitg/training/core/loggers/Log4JLogger.java
@@ -1,7 +1,11 @@
 package pl.sebitg.training.core.loggers;
 
 import org.apache.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier(value="log4j")
 public class Log4JLogger implements Logger {
 	
 	private static final org.apache.log4j.Logger LOG  = LogManager.getLogger(Log4JLogger.class);
