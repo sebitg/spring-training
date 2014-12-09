b61a8a70f92dd1fd2827191b09604c6443fb895a
Sebastian Mekal <s.mekal@samsung.com>
2014-12-09 09:16:33 +0100

    Task4 resolve

--- web-training/src/main/java/pl/sebitg/training/web/controllers/HomeController.java
+++ web-training/src/main/java/pl/sebitg/training/web/controllers/HomeController.java
@@ -2,16 +2,42 @@ package pl.sebitg.training.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-	
-	
-	@RequestMapping(value="/index", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String home(Model model) {
 		return "index";
+	}
+
+	@RequestMapping(value = "/somePage/{name}/{lastname}", method = RequestMethod.GET)
+	public String somePage1(Model model, @PathVariable("name") String name,
+			@PathVariable("lastname") String lastName) {
+		
+		model.addAttribute("fullname", name + " " + lastName);
+		if("Sebastian".equals(name)) {
+			model.addAttribute("vip", true);
+		}
+		
+		return "somepage";
+	}
+	
+	
+	@RequestMapping(value = "/somePage2", method = RequestMethod.GET)
+	public String somePage2(Model model, @RequestParam("name") String name,
+			@RequestParam("lastname") String lastName) {
+		
+		model.addAttribute("fullname", name + " " + lastName);
+		if("Sebastian".equals(name)) {
+			model.addAttribute("vip", true);
+		}
+		
+		return "somepage";
 	}
 
 }
